b5bbda04222fe5a691e191024450d4acfbf579bd
firssyameidelfia-hub <firssyameidelfia@gmail.com>
2026-06-28 18:32:44 +0700

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,18 +1,17 @@
 import os
-from google import genai
+import google.generativeai as genai
 from dotenv import load_dotenv
 
 # 1. Ambil setingan dari file .env
 load_dotenv()
 
 # 2. Siapkan koneksi ke Gemini
-client = genai.Client()
+genai.configure(api_key=os.environ.get("GEMINI_API_KEY"))
 
 # 3. Minta Gemini buat jawab pertanyaan
-response = client.models.generate_content(
-    model='gemini-2.5-flash',
-    contents='Halo Gemini! Saya sedang belajar membuat chatbot.',
-)
+model = genai.GenerativeModel('gemini-1.5-flash')
+
+response = model.generate_content('Halo Gemini! Saya sedang belajar membuat chatbot.')
 
 # 4. Tampilkan jawabannya di terminal
 print(response.text)
