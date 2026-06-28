54d7cd94beaae7ab7e386f518d32f21e886a02ff
firssyameidelfia-hub <firssyameidelfia@gmail.com>
2026-06-28 14:02:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def get_bot_response():
         
     try:
         # Menggunakan struktur pemanggilan Gemini versi stabil
-       model = genai.GenerativeModel('models/gemini-1.5-flash')
+      model = genai.GenerativeModel('gemini-2.5-flash')
         response = model.generate_content(user_text)
         
         # Pastikan dikembalikan dalam bentuk teks string murni untuk Flask
